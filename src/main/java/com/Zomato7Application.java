9db1a474ae6cca43f20a703c07d476c69cafb5a7
naitikjpatel <njpatel20031011@gmail.com>
2024-09-25 16:53:15 +0530

    initilized and added signup api

---
+++ src/main/java/com/Zomato7Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zomato7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zomato7Application.class, args);
+	}
+
+}
